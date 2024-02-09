248815880eb0e7fb1eb51d5d8c9bf0b6860f5738
akram mujawar <akram.mujawar@bluebenz.com>
2024-02-09 11:57:25 +0530

    solution

---
+++ src/webparts/treeView/components/ITreeViewProps.ts
@@ -0,0 +1,7 @@
+export interface ITreeViewProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/treeView/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ITreeViewWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'TreeViewWebPartStrings' {
+  const strings: ITreeViewWebPartStrings;
+  export = strings;
+}
